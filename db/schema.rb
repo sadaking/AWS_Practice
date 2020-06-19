7aefc3f0e712d20f696d48edf745df45450ed55b
sadaking <sichirigahama.37@gmail.com>
2020-06-19 17:12:16 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2020_06_18_051646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.bigint "user_id"
-    t.string "image"
-    t.index ["user_id"], name: "index_blogs_on_user_id"
-  end
-
   create_table "chats", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -30,14 +22,4 @@ ActiveRecord::Schema.define(version: 2020_06_18_051646) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
-  add_foreign_key "blogs", "users"
 end
